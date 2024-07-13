5e8f24632d333be742614dcea01aedd7af988780
Anton Kalyutich <anton.kalyutich@gmail.com>
2024-07-12 22:41:46 -0400

    Establish Basic Connection with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let request = await axios.get("http://127.0.0.1:8000/recipes/");
+        setRecipes(request.data);
+        console.log(recipes);
+        return request;
+      } catch (error) {
+        if (error.response) {
+          console.error("Error response:", error.response.data);
+        } else if (error.request) {
+          console.error("No response received:", error.request);
+        } else {
+          console.error("Axios error:", error.message);
+        }
+        console.error("Axios error config:", error.config);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe List</h1>
+      <p>Description Here</p>
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={index}>
+            <h2>{recipe.dish_name}</h2>
+            <p>{recipe.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
